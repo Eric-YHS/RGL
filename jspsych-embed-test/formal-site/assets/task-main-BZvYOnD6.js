7cdea0169ba37562076ffdbbbf65af31cfcb5089
Eric <eric@EricdeMacBook-Air.local>
2026-09-26 20:49:06 +0800

    Remove fangke from tutorial narration

--- jspsych-embed-test/formal-site/assets/task-main-BZvYOnD6.js
+++ jspsych-embed-test/formal-site/assets/task-main-BZvYOnD6.js
@@ -34,7 +34,7 @@ var it=Object.defineProperty;var st=(n,e,t)=>e in n?it(n,e,{enumerable:!0,config
     <p>请观看下面的示例短片，了解任务画面和操作方式。</p>
     <div class="instruction-video">
       <video controls preload="metadata" playsinline poster="./demo-annotated-25-12.svg">
-        <source src="./demo-natural-pauses-0925.mp4" type="video/mp4" />
+        <source src="./demo-no-fangke-0926.mp4" type="video/mp4" />
         当前浏览器无法直接播放示例短片。
       </video>
     </div>
